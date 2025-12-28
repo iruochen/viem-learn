d3433f5f4bf286413adce4b69831ad1b23ee3b3a
ruochen <wangrui@ruochen.email>
2025-12-28 16:29:01 +0800

    write contract

--- demo/src/index.ts
+++ demo/src/index.ts
@@ -5,6 +5,7 @@ import {
 	getContract,
 	http,
 	parseEther,
+	parseEventLogs,
 	publicActions,
 } from 'viem'
 import { foundry } from 'viem/chains'
@@ -12,9 +13,11 @@ import dotenv from 'dotenv'
 import { privateKeyToAccount } from 'viem/accounts'
 
 import ERC20_ABI from './abis/MyToken.json' with { type: 'json' }
+import COUNTER_ABI from './abis/Counter.json' with { type: 'json' }
 
 dotenv.config()
 const ERC20_ADDRESS = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9'
+const COUNTER_ADDRESS = '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853'
 
 const main = async () => {
 	const publicClient = createPublicClient({
@@ -73,6 +76,58 @@ const main = async () => {
 		})) as bigint,
 	)
 	console.log(`address ${userAddress[0]} has token balance: ${balance2}`)
+
+	const counterContract = getContract({
+		address: COUNTER_ADDRESS,
+		abi: COUNTER_ABI,
+		client: {
+			public: publicClient,
+			wallet: walletClient,
+		},
+	})
+
+	const tx = await counterContract.write?.increment?.()
+	console.log('Counter increment tx hash:', tx)
+
+	const receipt = await publicClient.waitForTransactionReceipt({
+		hash: tx as `0x${string}`,
+	})
+	// console.log('Transaction receipt:', receipt)
+
+	const number1 = await counterContract.read?.number?.()
+	console.log('Counter number after increment:', number1)
+
+	// write contract2
+	await walletClient.writeContract({
+		address: COUNTER_ADDRESS,
+		abi: COUNTER_ABI,
+		functionName: 'increment',
+		args: [],
+	})
+
+	const number2 = await publicClient.readContract({
+		address: COUNTER_ADDRESS,
+		abi: COUNTER_ABI,
+		functionName: 'number',
+		args: [],
+	})
+	console.log('Counter number after increment:', number2)
+
+	const tx2 = await erc20Contract.write?.transfer?.([
+		'0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+		parseEther('10'),
+	])
+	console.log('ERC20 transfer tx hash:', tx2)
+	const receipt2 = await publicClient.waitForTransactionReceipt({
+		hash: tx2 as `0x${string}`,
+	})
+	// console.log('ERC20 Transfer Transaction receipt:', receipt2)
+	const transferLogs = await parseEventLogs({
+		abi: ERC20_ABI,
+		eventName: 'Transfer',
+		logs: receipt2.logs,
+	})
+	console.log('Parsed Transfer event logs:', transferLogs)
 }
 
 main()
